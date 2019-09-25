f70c7c3b4f2714aab14ad21e86d4f08a0e4acbda
Christophe <c_parent@yahoo.com>
2019-09-25 10:23:28 +0200

    changing AMI

--- exemple_aws.tf
+++ exemple_aws.tf
@@ -1,9 +1,23 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "tfcloud"
+
+    workspaces {
+      name = "tf"
+    }
+  }
+}
+
+
+
 provider "aws" {
   region = "eu-west-3"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  // ami           = "ami-2757f631"
+  ami = "ami-04df694a94ac0b1b5"
   instance_type = "t2.micro"
 
   tags = {
@@ -11,7 +25,3 @@ resource "aws_instance" "example" {
     Group = "terraform"
   }
 }
-
-#resource "aws_autoscaling_group" "web" {
-# (resource arguments)
-#}
